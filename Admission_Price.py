5f79c02da508fa528e5c3b911cba255cbca064e7
Francesco Grande <78297879+GRAFRA346@users.noreply.github.com>
2021-02-18 14:30:46 +0100

    Update Admission_Price.py

--- Admission_Price.py
+++ Admission_Price.py
@@ -1,11 +1,19 @@
+print("""
+        LISTINO PREZZI ZOO
+        -Bambino <=2 anni: 0$
+        -Ragazzo <=12 anni: 14$
+        -Anziano 65+ anni: 18$
+        -Altri: 23$
+""")
+
 bambino: float = 0
 ragazzo: float = 14
 anziano: float = 18
 altro: float = 23
 
 eta_bambino: int = 2
-eta_ragazzo: int = 14
-eta_anziano: int = 23
+eta_ragazzo: int = 12
+eta_anziano: int = 65
 
 totale = 0
 
@@ -14,11 +22,11 @@ riga = input("Inserisci età del cliente(Riga vuota per terminare): ")
 while riga != "":
     eta = int(riga)
 
-    if eta <= 2:
+    if eta <= eta_bambino:
         totale += bambino
-    elif 3 <= eta <= 12:
+    elif eta <= eta_ragazzo:
         totale += ragazzo
-    elif eta >= 65:
+    elif eta >= eta_anziano:
         totale += anziano
     else:
         totale += altro
