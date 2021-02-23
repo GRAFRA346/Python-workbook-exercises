f45efc8b3a0a6acbffedd35e56a8ec6d2630722b
Francesco Grande <78297879+GRAFRA346@users.noreply.github.com>
2021-02-23 12:23:05 +0100

    Update 89.Convert_an_integer_to_Its_ordinal_number.py

--- 89.Convert_an_integer_to_Its_ordinal_number.py
+++ 89.Convert_an_integer_to_Its_ordinal_number.py
@@ -1,37 +1,18 @@
-
-
 def main():
     a = int(input("Enter a integer to convert, between 1 and 12: "))
     print("The ordinal number of this integer is: ", convert(a))
 
 
 def convert(ordinal):
-    if 1 < ordinal <= 12:
-        if ordinal == 1:
-            return "First"
-        elif ordinal == 2:
-            return "Second"
-        elif ordinal == 3:
-            return "Third"
-        elif ordinal == 4:
-            return "Fourth"
-        elif ordinal == 5:
-            return "Fifth"
-        elif ordinal == 6:
-            return "Sixth"
-        elif ordinal == 7:
-            return "Seventh"
-        elif ordinal == 8:
-            return "Eighth"
-        elif ordinal == 9:
-            return "Ninth"
-        elif ordinal == 10:
-            return "Tenth"
-        elif ordinal == 11:
-            return "Eleventh"
-        elif ordinal == 12:
-            return "Twelfth"
-        else:
-            return ""
+    # Dictionary 
+    d = {1: "First", 2: "Second", 3: "Third", 4: "Fourth",
+         5: "Fifth", 6: "Sixth", 7: "Seventh", 8: "Eighth",
+         9: "Ninth", 10: "Tenth", 11: "Eleventh", 12: "Twelfth"}
+    
+    if ordinal in d:
+        return d[ordinal]
+    else:
+        return ""
+
 
 main()
