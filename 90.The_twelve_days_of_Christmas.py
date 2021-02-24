f9ad549fa26be7a760d70b838b55f86331d58cb2
Francesco Grande <78297879+GRAFRA346@users.noreply.github.com>
2021-02-24 19:16:50 +0100

    Update 90.The_twelve_days_of_Christmas.py

--- 90.The_twelve_days_of_Christmas.py
+++ 90.The_twelve_days_of_Christmas.py
@@ -6,34 +6,34 @@ def displayVerse(n):
     print("my true love sent to me:")
 
     if n >= 12:
-        print("Twelve drummers drumming\n")
+        print("Twelve drummers drumming")
     elif n >= 11:
-        print("Eleven pipers piping\n")
+        print("Eleven pipers piping")
     elif n >= 10:
-        print("Ten lords a-leaping\n")
+        print("Ten lords a-leaping")
     elif n >= 9:
-        print("Nine ladies dancing\n")
+        print("Nine ladies dancing")
     elif n >= 8:
-        print("Eight maids a-milking\n")
+        print("Eight maids a-milking")
     elif n >= 7:
-        print("Seven swans a-swimming\n")
+        print("Seven swans a-swimming")
     elif n >= 6:
-        print("Six geese a-laying\n")
+        print("Six geese a-laying")
     elif n >= 5:
-        print("Five golden rings\n")
+        print("Five golden rings")
     elif n >= 4:
-        print("Four calling birds\n")
+        print("Four calling birds")
     elif n >= 3:
-        print("Three french hens\n")
+        print("Three french hens")
     elif n >= 2:
-        print("Two turtle doves, and\n")
+        print("Two turtle doves, and")
     elif n == 1:
-        print("partridge in a pear tree.\n")
+        print("partridge in a pear tree.")
 
 
 def main():
     for verse in range(1, 13):
-        print("[ Verse", verse, "]")
+        print("\n[ Verse", verse, "]")
         displayVerse(verse)
 
 
